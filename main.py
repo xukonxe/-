c7097fe5ec6fde4946bb732c2586476671117da4
xukonxe <1404084509@qq.com>
2024-05-04 13:48:30 +0800

    测试

--- main.py
+++ main.py
@@ -1,45 +1,23 @@
-'''
-Discord-Bot-Module template. For detailed usages,
- check https://interactions-py.github.io/interactions.py/
-
-Copyright (C) 2024  __retr0.init__
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-'''
 import interactions
 # Use the following method to import the internal module in the current same directory
 from . import internal_t
-# Import the os module to get the parent path to the local files
 import os
 # aiofiles module is recommended for file operation
 import aiofiles
-# You can listen to the interactions.py event
+# 监听interactions.py事件
 from interactions.api.events import MessageCreate
-# You can create a background task
+# 可以创建后台任务
 from interactions import Task, IntervalTrigger
 
-'''
-Replace the ModuleName with any name you'd like
-'''
-class ModuleName(interactions.Extension):
+
+class 五子棋十房间(interactions.Extension):
     module_base: interactions.SlashCommand = interactions.SlashCommand(
-        name="replace_your_command_base_here",
-        description="Replace here for the base command descriptions"
+        name="进入房间",
+        description="进入一个五子棋房间"
     )
     module_group: interactions.SlashCommand = module_base.group(
-        name="replace_your_command_group_here",
-        description="Replace here for the group command descriptions"
+        name="棋牌室_五子棋",
+        description="所有和五子棋有关的命令"
     )
 
     @module_group.subcommand("ping", sub_cmd_description="Replace the description of this command")
